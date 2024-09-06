e08d2c15b95882adcd5fe3c15d909727d5fb2cde
Vladyslav Zaiets <rutova2@gmail.com>
2024-09-06 06:40:15 +0300

    feat: add FluentConnectionStringBuilderJson json helpers
    should be backwards compatible.

--- src/FluentConnectionStringBuilderJsonExtensions.cs
+++ src/FluentConnectionStringBuilderJsonExtensions.cs
@@ -3,6 +3,9 @@ using System.Text.Json.Serialization.Metadata;
 
 namespace ConnStringDoctor;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="FluentConnectionStringBuilder"/>.
+/// </summary>
 public static class FluentConnectionStringBuilderJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
@@ -12,23 +15,25 @@ public static class FluentConnectionStringBuilderJsonExtensions
         WriteIndented = false,
     };
 
+    /// <summary>
+    /// Serializes a <see cref="FluentConnectionStringBuilder"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The connection string builder to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON representation of the connection string builder.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this FluentConnectionStringBuilder value, bool indented = false)
-    {
-        if (value == null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
+        => value is null
+            ? throw new ArgumentNullException(nameof(value))
+            : JsonSerializer.Serialize(value, indented
+                ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+                : _jsonOptions);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true,
-            }
-            : _jsonOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
-
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="FluentConnectionStringBuilder"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="FluentConnectionStringBuilder"/> instance if deserialization succeeds; otherwise, <see langword="null"/>.</returns>
     public static FluentConnectionStringBuilder? FromJson(string json)
     {
         if (string.IsNullOrWhiteSpace(json))
@@ -46,6 +51,12 @@ public static class FluentConnectionStringBuilderJsonExtensions
         }
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="FluentConnectionStringBuilder"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized builder if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
     public static bool TryFromJson(string json, out FluentConnectionStringBuilder? value)
     {
         value = null;
