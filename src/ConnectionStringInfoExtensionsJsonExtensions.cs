7e822c8deafb41d696d7be6447a0ce184cabc2e7
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 10:24:59 +0300

    polish ConnectionStringInfoExtensionsJsonExtensions

--- src/ConnectionStringInfoExtensionsJsonExtensions.cs
+++ src/ConnectionStringInfoExtensionsJsonExtensions.cs
@@ -5,69 +5,61 @@ namespace ConnStringDoctor;
 /// </summary>
 public static class ConnectionStringInfoExtensionsJsonExtensions
 {
-    private static readonly System.Text.Json.JsonSerializerOptions _jsonOptions = new(System.Text.Json.JsonSerializerOptions.Default)
-    {
-        PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase,
-        WriteIndented = false
-    };
+	private static readonly System.Text.Json.JsonSerializerOptions _jsonOptions = new(System.Text.Json.JsonSerializerOptions.Default)
+	{
+		PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase,
+		WriteIndented = false
+	};
 
-    /// <summary>
-    /// Serializes the <see cref="ConnectionStringInfo"/> instance to a JSON string.
-    /// </summary>
-    /// <param name="value">The connection string information to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the connection string information.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
-    public static string ToJson(this ConnectionStringInfo value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes the <see cref="ConnectionStringInfo"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The connection string information to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the connection string information.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+	public static string ToJson(this ConnectionStringInfo value, bool indented = false)
+		=> System.Text.Json.JsonSerializer.Serialize(value, indented ? new System.Text.Json.JsonSerializerOptions(_jsonOptions) { WriteIndented = true } : _jsonOptions);
 
-        var options = indented
-            ? new System.Text.Json.JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="ConnectionStringInfo"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>A deserialized <see cref="ConnectionStringInfo"/> instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+	/// <exception cref="System.Text.Json.JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static ConnectionStringInfo? FromJson(string json)
+	{
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return null;
+		}
 
-        return System.Text.Json.JsonSerializer.Serialize(value, options);
-    }
+		return System.Text.Json.JsonSerializer.Deserialize<ConnectionStringInfo>(json, _jsonOptions);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="ConnectionStringInfo"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A deserialized <see cref="ConnectionStringInfo"/> instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
-    /// <exception cref="System.Text.Json.JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static ConnectionStringInfo? FromJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="ConnectionStringInfo"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized instance if successful; otherwise, <see langword="null"/>.</param>
+	/// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+	public static bool TryFromJson(string json, out ConnectionStringInfo? value)
+	{
+		value = null;
 
-        return System.Text.Json.JsonSerializer.Deserialize<ConnectionStringInfo>(json, _jsonOptions);
-    }
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return false;
+		}
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="ConnectionStringInfo"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized instance if successful; otherwise, <see langword="null"/>.</param>
-    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
-    public static bool TryFromJson(string json, out ConnectionStringInfo? value)
-    {
-        value = null;
-
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return false;
-        }
-
-        try
-        {
-            value = System.Text.Json.JsonSerializer.Deserialize<ConnectionStringInfo>(json, _jsonOptions);
-            return true;
-        }
-        catch (System.Text.Json.JsonException)
-        {
-            return false;
-        }
-    }
+		try
+		{
+			value = System.Text.Json.JsonSerializer.Deserialize<ConnectionStringInfo>(json, _jsonOptions);
+			return true;
+		}
+		catch (System.Text.Json.JsonException)
+		{
+			return false;
+		}
+	}
 }
