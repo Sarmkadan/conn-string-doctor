4dcb003190620a645ae7e3297e54401cbfb6c15d
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 09:59:12 +0300

    polish ConnectionStringInfoJsonExtensions

--- src/ConnectionStringInfoJsonExtensions.cs
+++ src/ConnectionStringInfoJsonExtensions.cs
@@ -22,6 +22,7 @@ public static class ConnectionStringInfoJsonExtensions
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the connection string information.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown when serialization fails.</exception>
     public static string ToJson(this ConnectionStringInfo value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -38,9 +39,12 @@ public static class ConnectionStringInfoJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized connection string information, or null if the JSON is null or empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
     public static ConnectionStringInfo? FromJson(string json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         if (string.IsNullOrEmpty(json))
         {
             return null;
@@ -55,9 +59,12 @@ public static class ConnectionStringInfoJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized connection string information if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
     public static bool TryFromJson(string json, out ConnectionStringInfo? value)
     {
         value = null;
+
+        ArgumentNullException.ThrowIfNull(json);
 
         if (string.IsNullOrEmpty(json))
         {
